5c73c643128c03680e0989d93824ff56275aec57
sw.jung <kjss10@gmail.com>
2017-10-06 21:28:51 +0900

    Support swag helpers to compile_template

--- public/libs/compile_template.js
+++ public/libs/compile_template.js
@@ -1,31 +1,6 @@
 import { memoize } from 'lodash';
 import Handlebars from 'handlebars/dist/handlebars';
+import Swag from './swag';
 
-Handlebars.registerHelper({
-  eq: function (value, compareTo) {
-    return value === compareTo;
-  },
-  ne: function (value, compareTo) {
-    return value !== compareTo;
-  },
-  lt: function (value, compareTo) {
-    return value < compareTo;
-  },
-  gt: function (value, compareTo) {
-    return value > compareTo;
-  },
-  lte: function (value, compareTo) {
-    return value <= compareTo;
-  },
-  gte: function (value, compareTo) {
-    return value >= compareTo;
-  },
-  and: function (value, compareTo) {
-    return value && compareTo;
-  },
-  or: function (value, compareTo) {
-    return value || compareTo;
-  }
-});
-
+Swag.registerHelpers(Handlebars);
 export const compileTemplate = memoize(Handlebars.compile);
